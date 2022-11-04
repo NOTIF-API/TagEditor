f130bb7ebc2e90f0e594b25b90f38e4019383f3b
NOTIF-API <112265437+NOTIF-API@users.noreply.github.com>
2022-11-04 15:42:55 +0300

    Update Data.cs

--- Data.cs
+++ Data.cs
@@ -1,1 +1,1 @@
-{"version": "1.1.6", "update_next": "1.1.6 само обновление готово!", "promocode": "TagEditor-Serder", "limit": 500}
+{"version": 117, "update_next": "1.1.6 само обновление готово!", "promocode": "TagEditor-Serder", "limit": 500}
