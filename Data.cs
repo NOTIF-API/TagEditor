6c346a27351fd2e292fbc61286899b45fd6adfdb
NOTIF-API <112265437+NOTIF-API@users.noreply.github.com>
2022-12-20 16:50:50 +0300

    Update Data.cs

--- Data.cs
+++ Data.cs
@@ -1,1 +1,1 @@
-{"version": "120", "update_next": "1.2.0 Большое обновление меню и много чего нового", "promocode": "Notif-Love", "limit": 500}
+{"version": "121", "update_next": "1.2.0 больше сохронений", "promocode": "MORE-SAVE", "limit": 400}
